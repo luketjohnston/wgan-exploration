FIXED BUG in calculating r: was using logits as if they were log-probs, but that didn't work when logits were taken from different policies (because then the denominator of the softmax will differ between policies). Fixed this and now it seems to perform as well as stable-baselines-3, and it solves deterministic Pong.

ppo.py
@@ -73,48 +73,64 @@ returns:
 episode_rewards is a list of total reward for any episodes that terminated during
 the rollout.
 '''
-def rollout_GAE(vecenv, n, actor):
+def getRollout(vecenv, n, actor):
 
   # + 1 to hold extra state after last transition, to use to get final one-step td estimate
-  states = np.zeros([n + 1, vecenv.nenvs] + agent.INPUT_SHAPE)
+  states = np.zeros([n + 1, vecenv.nenvs] + agent.INPUT_SHAPE, np.float32)
   rewards = np.zeros([n, vecenv.nenvs])
   actions = np.zeros([n, vecenv.nenvs], dtype=np.int)
   dones = np.zeros([n, vecenv.nenvs], dtype=np.bool)
-  value_ests = np.zeros([n, vecenv.nenvs])
-  act_log_probs = np.zeros([n,vecenv.nenvs])
-  advs = np.zeros((n,vecenv.nenvs))
+
+  act_probs = np.zeros([n,vecenv.nenvs])
+
 
   episode_scores = []
 
   states[0] = vecenv.getStates()
 
   for i in range(n):
-    policy_logits, value_ests[i] = actor.policy_and_value(vecenv.getStates())
-    actions[i], act_log_probs[i] = actor.act(policy_logits)
+    policy_logits = actor.policy(vecenv.getStates())
+    actions[i], act_probs[i] = actor.act(policy_logits)
     states[i+1], rewards[i], dones[i], ep_scores = vecenv.step(actions[i])
     episode_scores += ep_scores
 
-  _, next_val = actor.policy_and_value(vecenv.getStates())
+  return (states, rewards, actions, dones, act_probs), episode_scores
+
+''' given rollout = (states, rewards, actions, dones, act_log_probs)
+as returned by getRollout, n = length of rollout, and actor, 
+returns the GAE adv estimates and returns,
+as well as the rest of the inputs needed for the batch of training
+'''
+
+def getGAE(rollout, n, actor):
+
+  (states, rewards, actions, dones, act_probs) = rollout
+  #states = np.reshape(states, (states.shape[0] * states.shape[1],) + states.shape[2:])
+  value_ests = tf.map_fn(actor.value, states)
+  #value_ests = np.reshape(value_ests, (-1, n) + value_ests.shape[1:])
+
+  advs = np.zeros((n,vecenv.nenvs))
+
   next_adv = 0.0
 
   # compute returns and advs using Generalized Advantage Estimation
   for t in reversed(range(n)):
     nonterminal = (1 - dones[t])
-    delta = rewards[t] + agent.DISCOUNT * next_val * nonterminal - value_ests[t]
+    delta = rewards[t] + agent.DISCOUNT * value_ests[t+1] * nonterminal - value_ests[t]
     advs[t] = delta + agent.LAMBDA * agent.DISCOUNT * next_adv * nonterminal 
     next_adv = advs[t]
-    next_val = value_ests[t]
+
+  states = states[:-1,...]          # toss last state and value estimage
+  value_ests = value_ests[:-1,...]  
 
   returns = advs + value_ests
-  state = states[-1,...] 
-  states = states[:-1,...]  # toss last state
 
   # reshape all the collected data into a single batch before returning
-  batch = [states, actions, returns, value_ests, act_log_probs]
+  batch = [states, actions, returns, value_ests, act_probs]
   for i,x in enumerate(batch):
     batch[i] = np.reshape(x, (n*vecenv.nenvs,) + x.shape[2:])
 
-  return batch, episode_scores
+  return batch
 
 
 
@@ -146,14 +162,15 @@ if __name__ == '__main__':
     if cycle == 3 and PROFILE:
       tf.profiler.experimental.stop()
 
-    rollout, episode_rewards = rollout_GAE(vecenv, ROLLOUT_LEN, actor)
-    #[states, actions, returns, value_ests, act_log_probs] = rollout
+    rollout, episode_rewards = getRollout(vecenv, ROLLOUT_LEN, actor)
+    #[states, rewards, actions, dones, act_log_probs] = rollout
 
     agentsave['episode_rewards'] += episode_rewards
 
     if len(agentsave['episode_rewards']) > 0:
-      av_ep_rew = sum(agentsave['episode_rewards'][-100:]) / len(agentsave['episode_rewards'][-100:])
+      av_ep_rew = sum(agentsave['episode_rewards'][-20:]) / len(agentsave['episode_rewards'][-20:])
       print('Average episode reward: ' + str(av_ep_rew))
+      print('Episode count: ' + str(len(agentsave['episode_rewards'])))
     
 
     print("Frames: %d" % ((cycle + 1) * ROLLOUT_LEN * ENVS))
@@ -177,11 +194,16 @@ if __name__ == '__main__':
     print('training ppo')
     total_datapoints = MINIBATCH_SIZE * MINIBATCHES
     indices = np.arange(total_datapoints)
+    # TODO: does it help sample complexity to recompute GAE at each epoch? some implementations do this (minimalRL), some dont (stable baselines 3)
+    # this paper https://arxiv.org/pdf/2006.05990.pdf recommends recomputing them for each epoch
+    # my current implementation of getGAE is really slow
+    batch = getGAE(rollout, ROLLOUT_LEN, actor)
     for e in range(EPOCHS):
+
       np.random.shuffle(indices)
       for mb_start in range(0,total_datapoints,MINIBATCH_SIZE):
         mb_indices = indices[mb_start:mb_start + MINIBATCH_SIZE]
-        inputs = [d[mb_indices,...] for d in rollout]
+        inputs = [d[mb_indices,...] for d in batch]
   
         loss_pve, grads, r, adv = actor.train(*inputs)
 